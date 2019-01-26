4a11a69d45fc6ba0af7ce57fb2632c3195db2fc9
tharikaGitHub <tharika.m92@gmail.com>
2019-01-26 10:49:06 +0530

    Fix OAuth test failure

--- distribution/scripts/jmeter/run-micro-gw-performance-tests.sh
+++ distribution/scripts/jmeter/run-micro-gw-performance-tests.sh
@@ -74,6 +74,8 @@ function before_execute_test_scenario() {
     jmeter_params+=("host=$apim_host" "port=$port" "path=$service_path")
     jmeter_params+=("payload=$HOME/${msize}B.json" "response_size=${msize}B" "protocol=$protocol"
         "tokens=$tokens")
+    echo "Starting APIM service"
+    ssh $apim_ssh_host "./apim/apim-start.sh -m $heap"
     echo "Starting APIM Micro-GW service"
     ssh $apim_ssh_host "./apim/micro-gw/micro-gw-start.sh -m $heap -n echo-mgw"
 }
